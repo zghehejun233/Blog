5f51739cca064d4801d373b61d4ccdb1e9e24d62
GuoSurui <thuray.email@gmail.com>
2024-12-10 23:24:33 +0800

    添加 Elog 同步

---
+++ elog.config.js
@@ -0,0 +1,37 @@
+const r2 = require("@elog/plugin-img-r2");
+
+module.exports = {
+  write: {
+    platform: "notion",
+    notion: {
+      token: process.env.NOTION_TOKEN,
+      databaseId: process.env.NOTION_DATABASE_ID,
+      filter: { property: "status", select: { equals: "已发布" } },
+    },
+  },
+  deploy: {
+    platform: "local",
+    local: {
+      outputDir: "./docs",
+      filename: "title",
+      format: "markdown",
+    },
+    frontMatter: {
+      enable: true,
+      include: [],
+      exclude: [],
+    },
+  },
+  image: {
+    enable: true,
+    plugin: r2,
+    r2: {
+      accessKeyId: process.env.R2_ACCESSKEYID,
+      secretAccessKey: process.env.R2_SECRET_ACCESSKEY,
+      bucket: process.env.R2_BUCKET,
+      endpoint: process.env.R2_ENDPOINT,
+      host: process.env.R2_HOST,
+      prefixKey: "blog/images/",
+    },
+  },
+};
